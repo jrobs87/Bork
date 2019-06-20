964e5b1c94d958534afa608a9c10c4a078e5b14a
David <davwil38@gmail.com>
2019-06-20 11:10:26 -0400

    -backend code cleanup
    -Added gitignore
    -removed keys.js file
    -removed extra code in apiRoutes.js
    -removed extra code in htmlRoutes.js
    -removed extra code in server.js

--- api/keys.js
+++
@@ -1,6 +0,0 @@
-console.log('keys are loaded');
-
-exports.petfinder = {
-  id: "PFC35OkaFrEhMUpk1jqb0gdjT4uaX85QgSdfuA5twyIErm3KFc",
-  secret: "8Vdl4QTZ3FjA7yDHmIrrLntLNYPxbKaZUcLXp4gT"
-}
--- server.js
+++ server.js
@@ -2,10 +2,6 @@ require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
 
-//var jwt = require("jwt-simple");
-
-var db = require("./models");
-
 var app = express();
 var PORT = process.env.PORT || 8080;
 
@@ -16,7 +12,7 @@ app.use(express.static("public"));
 
 // Handlebars
 app.engine("handlebars",
-  exphbs({ defaultLayout: "main"})
+  exphbs({ defaultLayout: "main" })
 );
 app.set("view engine", "handlebars");
 
@@ -24,25 +20,14 @@ app.set("view engine", "handlebars");
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-// var syncOptions = { force: false };
-
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-// if (process.env.NODE_ENV === "test") {
-//   syncOptions.force = true;
-// }
-
-// Starting the server, syncing our models ------------------------------------/
-// db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log('----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ')
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-    console.log('----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ')
-  });
-// });
+app.listen(PORT, function () {
+  console.log('----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ')
+  console.log(
+    "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+    PORT,
+    PORT
+  );
+  console.log('----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ')
+});
 
 module.exports = app;
