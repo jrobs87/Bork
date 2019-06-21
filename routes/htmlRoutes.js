bb45b37f3228f03191cad0776b17e84a65aa5a83
John Robertson <tiktaalikdesign@gmail.com>
2019-06-21 14:24:19 -0400

    consolidated routes to one file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -97,6 +97,22 @@ module.exports = function(app) {
     });
   });
 
+  // SWIPER API ROUTE TESTING
+  app.get("/api/swiper", function (req, res) {
+    db.swipe.findAll({}).then(function (dbswipe) {
+      res.send(dbswipe);
+
+      console.log('----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ')
+      console.log(`Dog Swiper API delivered with ${dbswipe.length} dog cards.  All matches below!`);
+
+      // Log out all dog matches.
+      for (i = 0; i < dbswipe.length; i++) {
+        console.log('----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ')
+        console.log(dbswipe[i].dataValues)
+      }
+    });
+  });
+
   app.get("*", function(req, res) {
     res.render("404")
   })
